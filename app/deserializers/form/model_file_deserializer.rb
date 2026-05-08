317ac455970cf4325f4bcb1ca0996b1a213ee4df
James Smith <james@floppy.org.uk>
2026-05-08 13:37:10 +0100

    fix i18n use

--- app/deserializers/form/model_file_deserializer.rb
+++ app/deserializers/form/model_file_deserializer.rb
@@ -5,7 +5,8 @@ module Form
       @params.require(:model_file).permit([
         :filename, # i18n-tasks-use t("activerecord.attributes.model_file.filename")
         :presupported, # i18n-tasks-use t("activerecord.attributes.model_file.presupported")
-        # printed isn't actually deserialized here # i18n-tasks-use t("activerecord.attributes.model_file.printed")
+        # printed isn't actually deserialized here but we'll include the form attribute anyway
+        # i18n-tasks-use t("activerecord.attributes.model_file.printed")
         :notes, # i18n-tasks-use t("activerecord.attributes.model_file.notes")
         :caption, # i18n-tasks-use t("activerecord.attributes.model_file.caption")
         :y_up, # i18n-tasks-use t("activerecord.attributes.model_file.y_up")
