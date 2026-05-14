0917a7d08e44bb7620a303108b01c764839e77a3
James Smith <james@floppy.org.uk>
2026-05-14 20:10:17 +0100

    Add test for setup process, redirection, etc

---
+++ spec/requests/setup_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.describe "First setup" do
+  [:singleuser, :multiuser].each do |mode|
+    context "when in #{mode} mode", mode do
+      context "when signed out" do
+        it "redirects to sign in" do
+          get "/"
+          expect(response).to redirect_to("/users/sign_in")
+        end
+
+        it "automatically signs in and redirects to user edit page" do
+          get "/users/sign_in"
+          follow_redirect!
+          expect(response).to redirect_to("/users/edit")
+        end
+      end
+
+      context "when signed in as new admin", :as_administrator do
+        [
+          "/dashboard",
+        ].each do |path|
+          it "redirects from #{path} to library creation" do
+            get path
+            expect(response).to redirect_to(new_library_path)
+          end
+        end
+      end
+    end
+  end
+end
